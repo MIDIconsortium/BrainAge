acb7939dce87eb2648d04869c9b563313448be94
MIDIconsortium <67752614+MIDIconsortium@users.noreply.github.com>
2022-02-02 00:27:49 +0000

    Update IXI_dataloader.py

--- IXI_dataloader.py
+++ IXI_dataloader.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    df = pd.read_excel('https://github.com/MIDIconsortium/BrainAge/blob/main/IXI.xls')
+    df = pd.read_excel(os.path.join(os.path.getcwd(),'IXI.xls'))
     df = df[~df['AGE'].isnull()].reset_index(drop=True)
     df = df.drop_duplicates(subset='IXI_ID', keep=False).reset_index(drop=True)
 
