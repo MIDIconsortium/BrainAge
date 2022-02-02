6a062fc6537d5cd8eb5c6bdcec99a9b31e71dcbd
MIDIconsortium <67752614+MIDIconsortium@users.noreply.github.com>
2022-02-02 01:07:50 +0000

    Update IXI_preprocess.py

--- IXI_preprocess.py
+++ IXI_preprocess.py
@@ -59,12 +59,30 @@ def preprocess(input_path):
         nib.save(new_image, os.path.join(save_path,str(int(ID[3:]))))
 
 if __name__ == "__main__":
-    input_path sys.argv[1:]
+    input_path, excel_path = sys.argv[1:]
     for root, dirs, files in os.walk(input_path):
         for f in files:
             nii_path = os.path.join(root, f)
             save_path = os.path.join(os.getcwd(),'IXI_nii')
             preprocess(nii_path, save_path)
+            
+    df = pd.read_excel(excel_path)
+    df = df[~df['AGE'].isnull()].reset_index(drop=True)
+    df = df.drop_duplicates(subset='IXI_ID', keep=False).reset_index(drop=True)
+
+    IDs = df['IXI_ID'].tolist()
+
+    paths = []
+    ages = []
+    nii_path = os.path.join(os.getcwd(),'IXI_NII')
+    for f in os.listdir(nii_path):
+        ID = f[:-3]
+        row = df[df['IXI_ID'].astype(int)==int(ID)]
+        age = int(row['AGE'])
+        paths.append(os.path.join(nii_path, f))
+        ages.append(age)
+
+    pd.DataFrame({'file_name':paths,'Age':ages}).to_csv(os.path.join(os.getcwd(),'IXI.csv'), index=False)
 
 
 
