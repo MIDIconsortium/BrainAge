c19c14ab15d7a918cdb3a6c2d960748786c7e674
MIDIconsortium <67752614+MIDIconsortium@users.noreply.github.com>
2022-02-02 01:24:30 +0000

    Update IXI_preprocess.py

--- IXI_preprocess.py
+++ IXI_preprocess.py
@@ -81,8 +81,10 @@ if __name__ == "__main__":
     ages = []
     nii_path = os.path.join(os.getcwd(),'IXI_nii')
     for f in os.listdir(nii_path):
-        ID = f[:-3]
-        row = df[df['IXI_ID'].astype(int)==int(ID)]
+        ID = int(f[:-4])
+        if ID not in IDs:
+            continue
+        row = df[df['IXI_ID'].astype(int)==ID]
         age = int(row['AGE'])
         paths.append(os.path.join(nii_path, f))
         ages.append(age)
