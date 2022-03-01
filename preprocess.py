32072f6fed80e39a026c0b8e61419cfe27cd4733
MIDIconsortium <67752614+MIDIconsortium@users.noreply.github.com>
2022-03-01 06:25:55 +0000

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -144,7 +144,7 @@ if __name__ == "__main__":
     parser.add_argument('--processed_nii_dir', type=str, default='./processed_nii/')
     parser.add_argument('--processed_csv_path', type=str, default='./brain_age_evaluation_dataset.csv')
     args = parser.parse_args()
-    csv_path, eval_csv_path, save_dir = args.csv_path, args. processed_csv_path, args.processed_nii_dir
+    csv_path, eval_csv_path, save_dir = args.raw_csv_path, args. processed_csv_path, args.processed_nii_dir
     os.mkdir(save_dir)
     df = pd.read_csv(csv_path)
     df['processed_file_name'] = -1
