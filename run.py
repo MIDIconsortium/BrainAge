518a22a6ef13769162ccfccbd7c2559b107761fa
MIDIconsortium <67752614+MIDIconsortium@users.noreply.github.com>
2022-03-01 23:43:56 +0000

    Update run.py

--- run.py
+++ run.py
@@ -109,6 +109,8 @@ if __name__ == "__main__":
     parser.add_argument('--gpu', dest='gpu', action='store_true')
     parser.set_defaults(gpu=False)
     parser.add_argument('--return_metrics', dest='return_metrics', action='store_true')
+    parser.add_argument('--processed_csv_file', type=str,  default='./brain_age_evaluation_dataset.csv')
+    
     parser.set_defaults(return_metrics=False)
     args = parser.parse_args()
     if args.gpu:
@@ -119,9 +121,9 @@ if __name__ == "__main__":
     net = net.to(device)
     eval_criterion = nn.L1Loss(reduction='sum')
     if args.return_metrics:
-        loader = get_IXI_test_loader('./IXI_test_dataset.csv', return_metrics=True)
+        loader = get_IXI_test_loader(args.processed_csv_file, return_metrics=True)
         loss, corr, true_ages, pred_ages, ID2pred, ID2truth = evaluate_with_age(net, loader, eval_criterion, device)
-        pd.DataFrame([ID2pred,ID2truth], index=['Predicted age (years)', 'True age (years)']).T.to_csv('./IXI_brain_age_predictions.csv',index=False)
+        pd.DataFrame([ID2pred,ID2truth], index=['Predicted age (years)', 'True age (years)']).T.to_csv('./T2_brain_age_predictions.csv',index=False)
         fig = plt.figure(figsize=(8,8))
         ax = fig.add_subplot(111)
         ax.scatter(true_ages, pred_ages, alpha=0.3)
@@ -131,11 +133,11 @@ if __name__ == "__main__":
         ax.set_xlabel('Chronological age')
         ax.set_ylabel('Predicted age')
         ax.set_title('MAE = {:.2f} years, p = {:.2f}\n'.format(loss, corr))
-        fig.savefig('./IXI_scatter.png', facecolor='w')
+        fig.savefig('./T2_scatter.png', facecolor='w')
     else:
-        loader = get_IXI_test_loader('./IXI_test_dataset.csv')
+        loader = get_IXI_test_loader(args.processed_csv_file)
         ID2pred = evaluate_without_age(net, loader, device)
-        pd.DataFrame(ID2pred, index=['Predicted age (years)']).T.to_csv('./IXI_brain_age_predictions.csv',index=False)
+        pd.DataFrame(ID2pred, index=['Predicted age (years)']).T.to_csv('./T2_brain_age_predictions.csv',index=False)
     
 
 
