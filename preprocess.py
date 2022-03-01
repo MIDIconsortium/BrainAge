1d54ed097fc818710c203bcac16006c04665dc03
MIDIconsortium <67752614+MIDIconsortium@users.noreply.github.com>
2022-03-01 06:36:03 +0000

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -62,6 +62,7 @@ def align_volume_to_ref(volume, aff, aff_ref=None, return_aff=False, n_dims=None
     # work on copy
     new_volume = volume.copy()
     aff_flo = aff.copy()
+    aff_flo = np.eye(4)
 
     # default value for aff_ref
     if aff_ref is None:
