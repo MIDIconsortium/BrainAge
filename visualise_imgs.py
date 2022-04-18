f056113603c7f6bfe2797d5cc505ffa773689910
MIDIconsortium <67752614+MIDIconsortium@users.noreply.github.com>
2022-04-18 04:38:16 +0100

    Create visualise_imgs.py

---
+++ visualise_imgs.py
@@ -0,0 +1,23 @@
+import numpy as np
+import nibabel as nib
+import monai
+from monai.transforms import (
+    AddChannel, 
+    Resize,
+    Spacing,
+    ResizeWithPadOrCrop
+)
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+import argparse
+import os
+import t2_skull_strip_and_preprocess
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nii_path', action='append')
+    args = parser.parse_args()
+    for path in parser.nii_path:
+        print(path
+    #preprocess(args.nii_path)
