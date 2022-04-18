fcba6f02b26d3e87fa26dbd1014309cd4099a2c3
MIDIconsortium <67752614+MIDIconsortium@users.noreply.github.com>
2022-04-18 04:47:46 +0100

    Update visualise_imgs.py

--- visualise_imgs.py
+++ visualise_imgs.py
@@ -14,10 +14,16 @@ import argparse
 import os
 import t2_skull_strip_and_preprocess
 if __name__ == "__main__":
-
+    if not os.path.exists('./processed_imgs/'):
+        osmkdir('./processed_imgs/')
     parser = argparse.ArgumentParser()
     parser.add_argument('--nii_path', nargs='*')
     args = parser.parse_args()
     for path in args.nii_path:
         print(path)
-    #preprocess(args.nii_path)
+        arr = t2_skull_strip_and_preprocess.preprocess(args.nii_path)
+        fig, (ax1, ax2) = plt.subplots(1,2, figsize=(12,12))
+        ax1.imshow(arr.squeeze()[65,:,:], cmap='gray')
+        ax2.imshow(arr.squeeze()[:,:,65], cmap='gray')
+        fig.savefig('./processed_imgs/{}.png'.format(len(os.listdir('./processed_imgs/')))
+        plt.close()
