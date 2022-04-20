61feff30c270890bb7cd853af0ddce11dd46b636
MIDIconsortium <67752614+MIDIconsortium@users.noreply.github.com>
2022-04-20 09:10:29 +0100

    Update visualise_processed_scans.py

--- visualise_processed_scans.py
+++ visualise_processed_scans.py
@@ -13,16 +13,31 @@ warnings.filterwarnings("ignore")
 import argparse
 import os
 import t2_skull_strip_and_preprocess
+import t1_skull_strip_register_and_preprocess
+import preprocess
+
 if __name__ == "__main__":
     if not os.path.exists('./processed_imgs/'):
         os.mkdir('./processed_imgs/')
     parser = argparse.ArgumentParser()
     parser.add_argument('--nii_path', nargs='*')
+    parser.add_argument('--skull_strip', dest='skull_strip', action='store_true')
+    parser.set_defaults(skull_strip=False)
+    parser.add_argument('--sequence', type=str, default='t2')
     args = parser.parse_args()
     for path in args.nii_path:
-        arr = t2_skull_strip_and_preprocess.preprocess(path)
+        if args.sequence == 't2' and not args.skull_strip:
+            processed_arr = preprocess.preprocess(file_name)
+        elif args.sequence == 't2' and args.skull_strip:
+            processed_arr = t2_skull_strip_and_preprocess.preprocess(file_name, args.gpu)
+        elif args.sequence == 't1' and args.skull_strip:
+            t1_skull_strip_register_and_preprocess.preprocess(file_name, args.gpu)
+        else:
+            raise ValueError('MRI sequence {} (skull_strip: {}) not currently handled'.format(args.sequence, args.skull_strip))
+        if not type(processed_arr)==np.ndarray:
+            continue
         fig, (ax1, ax2) = plt.subplots(1,2, figsize=(12,12))
-        ax1.imshow(np.rot90(arr.squeeze()[65,:,:]), cmap='gray')
+        ax1.imshow(np.rot90(processed_arr.squeeze()[65,:,:]), cmap='gray')
         ax2.imshow(arr.squeeze()[:,:,65], cmap='gray')
         fig.savefig('./processed_imgs/{}.png'.format(len(os.listdir('./processed_imgs/'))))
         plt.close()
